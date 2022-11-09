55c0c9aea7c710afbe1adf491e40b70f6ff6bae4
antiguaMob <antigua_m@mail.ru>
2022-11-09 03:29:15 +0300

    settings menu is ready

--- app/src/main/java/com/antigua/mytelegram/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/antigua/mytelegram/ui/fragments/SettingsFragment.kt
@@ -8,6 +8,7 @@ class SettingsFragment :  BaseFragment(R.layout.fragment_settings) {
 
     override fun onResume() {
         super.onResume()
+        setHasOptionsMenu(true)
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
