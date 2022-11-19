1eb11ca76d00fb9ca12e90ad0b7d6feb859f6f79
antiguaMob <antigua_m@mail.ru>
2022-11-19 17:25:29 +0300

    fix bugs null states

--- app/src/main/java/com/antigua/mytelegram/utilits/funs.kt
+++ app/src/main/java/com/antigua/mytelegram/utilits/funs.kt
@@ -1,13 +1,16 @@
 package com.antigua.mytelegram.utilits
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.provider.ContactsContract
 import android.view.inputmethod.InputMethodManager
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.antigua.mytelegram.R
+import com.antigua.mytelegram.models.CommonModel
 import com.antigua.mytelegram.utilits.AppConstants.APP_ACTIVITY
 import com.squareup.picasso.Picasso
 
@@ -51,4 +54,33 @@ fun ImageView.downloadAndSetImage(url: String){
         .fit()
         .placeholder(R.drawable.default_photo)
         .into(this)
+}
+
+@SuppressLint("Range")
+fun initContacts() {
+    if(checkPermission(READ_CONTACTS)){
+        val arrayContacts = arrayListOf<CommonModel>()
+        val cursor = APP_ACTIVITY.contentResolver.query(
+            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+            null,
+            null,
+            null,
+            null,
+            null,
+        )
+        cursor?.let {
+            while (it.moveToNext()){
+                val fullName = it.getString(it.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))
+                val phone = it.getString(it.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
+                val newModel = CommonModel()
+                newModel.fullname = fullName
+                newModel.phone = phone.replace(Regex("[\\s,-]"),"")
+                arrayContacts.add(newModel)
+            }
+        }
+        cursor?.close()
+        updatePhonesToDatabase(arrayContacts)
+
+
+    }
 }
