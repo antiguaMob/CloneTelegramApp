3c009c217e9f52cb8009ea491b98202a5700cd18
antiguaMob <antigua_m@mail.ru>
2022-11-20 03:40:53 +0300

    Optimization of RecycleView in chat. Using the DiffUtil utility.

--- app/src/main/java/com/antigua/mytelegram/utilits/AppChildEventListener.kt
+++ app/src/main/java/com/antigua/mytelegram/utilits/AppChildEventListener.kt
@@ -10,18 +10,18 @@ class AppChildEventListener (val onSuccess:(DataSnapshot)->Unit):ChildEventListe
     }
 
     override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
-       // TODO("Not yet implemented")
+//        TODO("Not yet implemented")
     }
 
     override fun onChildRemoved(snapshot: DataSnapshot) {
-       // TODO("Not yet implemented")
+//        TODO("Not yet implemented")
     }
 
     override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
-       // TODO("Not yet implemented")
+//        TODO("Not yet implemented")
     }
 
     override fun onCancelled(error: DatabaseError) {
-        //TODO("Not yet implemented")
+//        TODO("Not yet implemented")
     }
 }
